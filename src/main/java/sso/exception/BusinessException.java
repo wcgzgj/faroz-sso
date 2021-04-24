14513f218a38ac2739f6878c097f2e8130d5383b
wcgzgj <zhangjia_good@126.com>
2021-04-24 16:42:39 +0800

    自定义异常

---
+++ src/main/java/sso/exception/BusinessException.java
@@ -0,0 +1,37 @@
+package sso.exception;
+
+/**
+ * @ClassName BusinessException
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/4/24 下午4:41
+ * @Version 1.0
+ **/
+public class BusinessException extends RuntimeException{
+    private BusinessExceptionCode code;
+
+    public BusinessException (BusinessExceptionCode code) {
+        /**
+         * 父类有个成员变量  private String desc
+         * 需要为这个成员变量赋值
+         */
+        super(code.getDesc());
+        this.code = code;
+    }
+
+    public BusinessExceptionCode getCode() {
+        return code;
+    }
+
+    public void setCode(BusinessExceptionCode code) {
+        this.code = code;
+    }
+
+    /**
+     * 不写入堆栈信息，提高性能
+     */
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
+}
