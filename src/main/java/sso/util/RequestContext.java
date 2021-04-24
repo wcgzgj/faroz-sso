35540203ee31325423787eac53fcad35f7a3d438
wcgzgj <zhangjia_good@126.com>
2021-04-24 16:39:54 +0800

    aop

---
+++ src/main/java/sso/util/RequestContext.java
@@ -0,0 +1,22 @@
+package sso.util;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName RequestContext
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/4/24 下午4:37
+ * @Version 1.0
+ **/
+public class RequestContext implements Serializable {
+    private static ThreadLocal<String> remoteAddr = new ThreadLocal<>();
+
+    public static String getRemoteAddr() {
+        return remoteAddr.get();
+    }
+
+    public static void setRemoteAddr(String remoteAddr) {
+        RequestContext.remoteAddr.set(remoteAddr);
+    }
+}
